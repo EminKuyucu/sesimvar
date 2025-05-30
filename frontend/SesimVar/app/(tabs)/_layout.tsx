26e82f1dcda810ee2fb086c63475743b5724ea59
yagmurekc <ekiciyagmurr@gmail.com>
2025-05-30 17:45:51 +0300

    Merbaa

--- frontend/SesimVar/app/(tabs)/_layout.tsx
+++ frontend/SesimVar/app/(tabs)/_layout.tsx
@@ -1,16 +1,16 @@
-import { Tabs } from 'expo-router';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { Slot } from 'expo-router';
 
-export default function TabLayout() {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{ title: 'Home' }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{ title: 'Explore' }}
-      />
-    </Tabs>
-  );
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <Slot />;
 }
